0acc4dcf3a524ebd4a4474f9dd1ff6736b5a5119
Birapcampos <birapcampos@gmail.com>
2024-06-02 21:31:15 -0300

    Primeiro commit

---
+++ src/main/java/br/com/birapcampos/webflux_exemplo/WebfluxExemploApplication.java
@@ -0,0 +1,13 @@
+package br.com.birapcampos.webflux_exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxExemploApplication.class, args);
+	}
+
+}
