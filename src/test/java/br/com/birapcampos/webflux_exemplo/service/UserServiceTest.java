74e2d19839ec8dbcce56a6e437c916036c97354d
Birapcampos <birapcampos@gmail.com>
2024-06-05 09:45:10 -0300

    Add tests to UserService findAll and update

--- src/test/java/br/com/birapcampos/webflux_exemplo/service/UserServiceTest.java
+++ src/test/java/br/com/birapcampos/webflux_exemplo/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -56,8 +57,7 @@ class UserServiceTest {
     @Test
     void testFindById(){
 
-        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().id("1234")
-                .build()));
+        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().build()));
 
         Mono<User> result = service.findById("12345");
 
@@ -69,5 +69,40 @@ class UserServiceTest {
         Mockito.verify(repository,times(1)).findById(anyString());
     }
 
+    @Test
+    void testFindAll(){
+
+        when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+
+        Flux<User> result = service.findAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete().verify();
+
+
+        Mockito.verify(repository,times(1)).findAll();
+    }
+
+    @Test
+    void testUpdate(){
+
+        UserRequest request = new UserRequest("Bira","bira@exmplo.com","123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class),any(User.class))).thenReturn(entity);
+        when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+        when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.update("123",request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository,times(1)).save(any(User.class));
+
+    }
 
 }
